e364cf2f92cde3db00b83f9914fc340d297a5cda
nastyadunaeva <56322861+nastyadunaeva@users.noreply.github.com>
2021-05-22 23:43:39 +0300

    Add files via upload

--- ViewModelTests/UnitTest1.cs
+++ ViewModelTests/UnitTest1.cs
@@ -10,14 +10,14 @@ namespace ViewModelTests
         [Fact]
         public void HasUnsavedChangesTest()
         {
-            MainViewModel model = new MainViewModel();
+            MainViewModel model = new MainViewModel(new TestUIServices("", ""));
             model.AddDefaults();
             Assert.True(model.HasUnsavedChanges());
         }
         [Fact]
         public void AddTest()
         {
-            MainViewModel model = new MainViewModel();
+            MainViewModel model = new MainViewModel(new TestUIServices("", ""));
             Assert.Equal(0, model.Coll.Count);
             model.AddDefaultGrid();
             Assert.Equal(1, model.Coll.Count);
@@ -27,7 +27,7 @@ namespace ViewModelTests
         [Fact]
         public void RemoveAllTest()
         {
-            MainViewModel model = new MainViewModel();
+            MainViewModel model = new MainViewModel(new TestUIServices("", ""));
             Assert.True(model.ItemIsNull());
             Assert.NotNull(model.Coll);
             model.AddDefaults();
@@ -37,7 +37,7 @@ namespace ViewModelTests
         [Fact]
         public void MaxDistTest()
         {
-            MainViewModel model = new MainViewModel();
+            MainViewModel model = new MainViewModel(new TestUIServices("", ""));
             model.AddDefaultGrid();
             Assert.True(model.Max_dist != 0);
             model.RemoveAt(0);
@@ -50,5 +50,26 @@ namespace ViewModelTests
             Assert.Equal("Error data", item.Error);
             Assert.Equal("Field must be positive", item["Field"]);
         }
+        [Fact]
+        public void DoNotOpenTest()
+        {
+            MainViewModel model = new MainViewModel(new TestUIServices("No filename", "Error"));
+            model.OpenCommand.Execute(null);
+            Assert.False(model.Coll.Is_changed);
+        }
+    }
+    public class TestUIServices : IUIServices
+    {
+        private readonly string filenameSave; // имя файла, выбранного при сохранении
+        private readonly string filename; // имя выбранного в диалоге файла
+
+        public TestUIServices(string b = "No filename", string c = "Error") 
+        {
+            filenameSave = b;
+            filename = c;
+        }
+
+        public string SelectFileSave() => filenameSave;
+        public string SelectFile() => filename;
     }
 }
